c8c9a7cfd866de3b2eba0d9c7ffad671c0b35ab8
decentparadox <rsasank69@gmail.com>
2024-12-14 00:05:19 +0530

    i am not sure whats happening

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is static.cloudflareinsights.com plausible.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is static.cloudflareinsights.com plausible.io www.googletagmanager.com www.google-analytics.com vercel.live;
   style-src 'self' 'unsafe-inline' fonts.googleapis.com;
   img-src * blob: data:;
   media-src 'self';
